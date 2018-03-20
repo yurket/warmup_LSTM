ae2b571fbfb1d7668a75c9bab19234e9c14b1c3f
yurket <yurket2006@yandex.ru>
2018-03-21 00:57:17 +0300

    Working on...

--- bit_parity.py
+++ bit_parity.py
@@ -12,9 +12,9 @@ import tensorflow as tf
 # 50 bits long string
 INPUT_SIZE = 50
 N_TRAIN_SAMPLES = 102400
-HIDDEN_UNITS = 1
-BATCH_SIZE = 256
-LEARNING_RATE = 0.5
+HIDDEN_UNITS = 8
+BATCH_SIZE = 1024
+LEARNING_RATE = 0.005
 
 TRAINING_STEPS = 1000
 DISPLAY_STEP = TRAINING_STEPS / 100
@@ -25,7 +25,8 @@ TENSORBOARD_DIR = "./tensorboard"
 def generate_dataset(n=N_TRAIN_SAMPLES):
     max_50_bit_number = 2**50 - 1
     random_numbers = np.random.randint(0, max_50_bit_number, n)
-    X_strings = map(lambda x: "{:050b}".format(x), random_numbers)
+    X_strings = list(map(lambda x: "{:050b}".format(x), random_numbers))
+    # print(X_strings)
     y = []
     for x_s in X_strings:
         if x_s.count('1') % 2 == 0:
@@ -43,10 +44,12 @@ def create_rnn_cell(cell_type='rnn'):
         return tf.nn.rnn_cell.GRUCell(num_units=HIDDEN_UNITS)
     elif cell_type == 'lstm':
         return tf.nn.rnn_cell.BasicLSTMCell(num_units=HIDDEN_UNITS)
+        # return tf.nn.rnn_cell.BasicLSTMCell(num_units=HIDDEN_UNITS, state_is_tuple=True)
 
 
 def build_and_train_model(dataset):
     print("[..] Building network model")
+    # TODO: replace with None?
     X = tf.placeholder(tf.float32, (BATCH_SIZE, INPUT_SIZE), name="X")
     y = tf.placeholder(tf.float32, (BATCH_SIZE, 2), name="y")
 
@@ -59,15 +62,25 @@ def build_and_train_model(dataset):
     #             tf.get_variable_scope().reuse_variables()
     #         _, state = rnn_cell(X[:, i*HIDDEN_UNITS:(i+1)*HIDDEN_UNITS], state)
 
-    rnn_cell = create_rnn_cell('rnn')
-    outputs, final_state = tf.nn.static_rnn(rnn_cell, )
+    lstm_cell = create_rnn_cell('lstm')
+
+    # current_state = tf.zeros([BATCH_SIZE, HIDDEN_UNITS])
+    # hidden_state = tf.zeros([BATCH_SIZE, HIDDEN_UNITS])
+    # init_state = tf.nn.rnn_cell.LSTMStateTuple(current_state, hidden_state)
+
+    inputs = tf.reshape(X, [BATCH_SIZE, INPUT_SIZE, 1])
+    inputs = tf.unstack(inputs, INPUT_SIZE, axis=1)
+    # outputs, final_state = tf.nn.static_rnn(lstm_cell, inputs, initial_state=init_state)
+    outputs, final_state = tf.nn.static_rnn(lstm_cell, inputs, dtype=tf.float32)
+    # print("Outputs: ", len(outputs))
+    # print("Final state: ", final_state)
 
     with tf.name_scope("Logits"):
         Why = tf.get_variable("Why", shape=[HIDDEN_UNITS, 2],
                               initializer=tf.contrib.layers.xavier_initializer())
         b = tf.get_variable("b", shape=[2,],
                             initializer=tf.contrib.layers.xavier_initializer())
-        logits = tf.matmul(final_state, Why) + b
+        logits = tf.matmul(outputs[-1], Why) + b
 
     with tf.name_scope("CE"):
         loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=logits))
@@ -88,6 +101,9 @@ def build_and_train_model(dataset):
 
     print("[..] Training the model")
     X_train, y_train = np.array(dataset[0]), np.array(dataset[1])
+    print("X_train: {}\n".format(X_train))
+    print("y_train: {}\n".format(y_train))
+
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
@@ -98,14 +114,15 @@ def build_and_train_model(dataset):
         for step in range(TRAINING_STEPS):
             i = 0
             for batch_start in range(0, N_TRAIN_SAMPLES, BATCH_SIZE):
+                batch_end = batch_start + BATCH_SIZE
                 _, loss_val, acc_val = sess.run([train_op, loss_op, accuracy_op],
-                                       {X: X_train[batch_start : batch_start + BATCH_SIZE],
-                                        y: y_train[batch_start : batch_start + BATCH_SIZE]})
+                                       {X: X_train[batch_start : batch_end],
+                                        y: y_train[batch_start : batch_end]})
 
                 i += 1
                 if i % 5 == 0:
-                    s = sess.run(summ, feed_dict={X: X_train[batch_start : batch_start + BATCH_SIZE],
-                                                  y: y_train[batch_start : batch_start + BATCH_SIZE]})
+                    s = sess.run(summ, feed_dict={X: X_train[batch_start : batch_end],
+                                                  y: y_train[batch_start : batch_end]})
                     writer.add_summary(s, step)
 
             if (step+1) % DISPLAY_STEP == 0:
@@ -122,6 +139,7 @@ def clean_logs_from_previous_run():
 def main():
     clean_logs_from_previous_run()
     dataset = generate_dataset(N_TRAIN_SAMPLES)
+    # print(dataset)
     train_op = build_and_train_model(dataset)
 
 
