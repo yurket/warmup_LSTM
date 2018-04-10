b3762b0161a118f0f921f84921454717a44e0d22
yurket <yurket2006@yandex.ru>
2018-04-11 00:04:25 +0300

    Cleaner sample

--- bit_parity.py
+++ bit_parity.py
@@ -9,12 +9,12 @@ import numpy as np
 import tensorflow as tf
 
 
-# 50 bits long string
 INPUT_SIZE = 50
-N_TRAIN_SAMPLES = 102400
-HIDDEN_UNITS = 8
-BATCH_SIZE = 1024
-LEARNING_RATE = 0.005
+N_TRAIN_SAMPLES = 10240
+# just random guess of 16 lstm cells
+HIDDEN_UNITS = 16
+BATCH_SIZE = 256
+LEARNING_RATE = 0.05
 
 TRAINING_STEPS = 1000
 DISPLAY_STEP = TRAINING_STEPS / 100
@@ -37,43 +37,23 @@ def generate_dataset(n=N_TRAIN_SAMPLES):
     X = [[int(i) for i in s] for s in X_strings]
     return (X, y)
 
-def create_rnn_cell(cell_type='rnn'):
-    if cell_type == 'rnn':
-        return tf.nn.rnn_cell.BasicRNNCell(num_units=HIDDEN_UNITS)
-    elif cell_type == 'gru':
-        return tf.nn.rnn_cell.GRUCell(num_units=HIDDEN_UNITS)
-    elif cell_type == 'lstm':
-        return tf.nn.rnn_cell.BasicLSTMCell(num_units=HIDDEN_UNITS)
-        # return tf.nn.rnn_cell.BasicLSTMCell(num_units=HIDDEN_UNITS, state_is_tuple=True)
-
 
 def build_and_train_model(dataset):
     print("[..] Building network model")
-    # TODO: replace with None?
+
+    # input tensor X is #{BATCH_SIZE} of 50bits lists
     X = tf.placeholder(tf.float32, (BATCH_SIZE, INPUT_SIZE), name="X")
+    # the answer is either even [1, 0] or odd [0, 1] 
     y = tf.placeholder(tf.float32, (BATCH_SIZE, 2), name="y")
 
-    # rnn_cell = create_rnn_cell('rnn')
-    # rnn_iterations = INPUT_SIZE / HIDDEN_UNITS
-    # state = tf.zeros([BATCH_SIZE, HIDDEN_UNITS])
-    # with tf.variable_scope('RNN'):
-    #     for i in range(rnn_iterations):
-    #         if i > 0:
-    #             tf.get_variable_scope().reuse_variables()
-    #         _, state = rnn_cell(X[:, i*HIDDEN_UNITS:(i+1)*HIDDEN_UNITS], state)
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(num_units=HIDDEN_UNITS)
 
-    lstm_cell = create_rnn_cell('lstm')
-
-    # current_state = tf.zeros([BATCH_SIZE, HIDDEN_UNITS])
-    # hidden_state = tf.zeros([BATCH_SIZE, HIDDEN_UNITS])
-    # init_state = tf.nn.rnn_cell.LSTMStateTuple(current_state, hidden_state)
-
+    # cast inputs to the shape `static_rnn` wants: list of 50 elements every of size [BATCH_SIZE, 1]
     inputs = tf.reshape(X, [BATCH_SIZE, INPUT_SIZE, 1])
     inputs = tf.unstack(inputs, INPUT_SIZE, axis=1)
-    # outputs, final_state = tf.nn.static_rnn(lstm_cell, inputs, initial_state=init_state)
+    assert(len(inputs) == INPUT_SIZE)
+    assert(inputs[0].shape == (BATCH_SIZE, 1) )
     outputs, final_state = tf.nn.static_rnn(lstm_cell, inputs, dtype=tf.float32)
-    # print("Outputs: ", len(outputs))
-    # print("Final state: ", final_state)
 
     with tf.name_scope("Logits"):
         Why = tf.get_variable("Why", shape=[HIDDEN_UNITS, 2],
@@ -87,22 +67,19 @@ def build_and_train_model(dataset):
         tf.summary.scalar("CE_loss", loss_op)
 
     with tf.name_scope("accuracy"):
-        assert(tf.argmax(logits, axis=1).shape == tf.argmax(y, axis=1).shape)
-        correct_predictions = tf.equal(tf.argmax(logits, axis=1), tf.argmax(y, axis=1))
+        predictions = tf.nn.softmax(logits)
+        assert(tf.argmax(predictions, axis=1).shape == tf.argmax(y, axis=1).shape)
+        correct_predictions = tf.equal(tf.argmax(predictions, axis=1), tf.argmax(y, axis=1))
         accuracy_op = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
         tf.summary.scalar("accuracy", accuracy_op)
 
     with tf.name_scope("train"):
         train_op = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss_op)
 
-
     summ = tf.summary.merge_all()
-
 
     print("[..] Training the model")
     X_train, y_train = np.array(dataset[0]), np.array(dataset[1])
-    print("X_train: {}\n".format(X_train))
-    print("y_train: {}\n".format(y_train))
 
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
@@ -116,8 +93,8 @@ def build_and_train_model(dataset):
             for batch_start in range(0, N_TRAIN_SAMPLES, BATCH_SIZE):
                 batch_end = batch_start + BATCH_SIZE
                 _, loss_val, acc_val = sess.run([train_op, loss_op, accuracy_op],
-                                       {X: X_train[batch_start : batch_end],
-                                        y: y_train[batch_start : batch_end]})
+                                       feed_dict= {X: X_train[batch_start : batch_end],
+                                                   y: y_train[batch_start : batch_end]})
 
                 i += 1
                 if i % 5 == 0:
@@ -140,7 +117,7 @@ def main():
     clean_logs_from_previous_run()
     dataset = generate_dataset(N_TRAIN_SAMPLES)
     # print(dataset)
-    train_op = build_and_train_model(dataset)
+    build_and_train_model(dataset)
 
 
 if __name__ == '__main__':
